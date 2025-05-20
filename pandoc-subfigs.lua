9ef10a0068934dbd991699a4eb428ebcd118ae77
R. N. West <98110034+rnwst@users.noreply.github.com>
2025-05-20 23:56:43 +0100

    Use stylua for code formatting

--- pandoc-subfigs.lua
+++ pandoc-subfigs.lua
@@ -1,27 +1,20 @@
 local function has_value(tab, val)
-    for _, v in ipairs(tab) do
-        if v == val then
-            return true
-        end
-    end
-    return false
+   for _, v in ipairs(tab) do
+      if v == val then return true end
+   end
+   return false
 end
-
 
 local function first_para_idx(blocks)
-    for index, block in ipairs(blocks) do
-        if block.tag == 'Para' then
-            return index
-        end
-    end
+   for index, block in ipairs(blocks) do
+      if block.tag == 'Para' then return index end
+   end
 end
 
-
 function Div(elt)
-    local caption_idx = first_para_idx(elt.content)
-    if has_value(elt.classes, 'subfigs') and caption_idx then
-        local caption =
-            pandoc.Plain(table.remove(elt.content, caption_idx).content)
-        return pandoc.Figure(elt.content, {long=caption}, elt.attr)
-    end
+   local caption_idx = first_para_idx(elt.content)
+   if has_value(elt.classes, 'subfigs') and caption_idx then
+      local caption = pandoc.Plain(table.remove(elt.content, caption_idx).content)
+      return pandoc.Figure(elt.content, { long = caption }, elt.attr)
+   end
 end
